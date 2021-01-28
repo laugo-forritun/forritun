e76ba190f12591a2b43f61d497646868bb3da2f5
laugo-forritun <77778082+laugo-forritun@users.noreply.github.com>
2021-01-28 10:01:15 +0000

    client.py: Updated to completely convert to blocks

--- client.py
+++ client.py
@@ -1,54 +1,51 @@
 ##########################################
 ### Helper functions for clients       ###
 ##########################################
+""" Handle messages """
 def on_received_value(ID_recv, n_recv):
-    """ Handle messages """
     if int(ID_recv) == ID:
         queue.append(n_recv)
 radio.on_received_value(on_received_value)
+""" Write (send) `n` to server """
 def write(n: number):
-    """ Write (send) `n` to server """
     radio.send_value(str(ID), n)
+""" Blocking read from server """
 def read():
-    """ Blocking read from server """
     while len(queue) == 0:
         basic.pause(100)
     return queue.shift()
+""" Win/lose handling """
 def on_received_string(_s):
-    """ Win/lose handling """
     _i = 0
     for c in _s:
         if c == ' ': break
         _i += 1
-    if int(_s[:_i]) != ID: return
-    _s = _s[_i+1:]
-    if _s == "WIN":
-        on_win()
-    elif _s == "LOSS":
-        on_loss()
-    else: basic.show_icon(IconNames.CONFUSED)
+    if int(_s.substr(0,_i)) == ID:
+        _s = _s.substr(_i+1, len(_s))
+        if _s == "WIN":
+            on_win()
+        elif _s == "LOSS":
+            on_loss()
+        else: basic.show_icon(IconNames.CONFUSED)
 radio.on_received_string(on_received_string)
 
-# Required objects
-radio.set_group(1)
+
+ID = 0
 queue: List[number] = []
+queue = []
 ID = randint(0, 1000000)
+radio.set_group(1)
 
 
 
 ##########################################
 ### Functions for clients to implement ###
 ##########################################
+""" Code handling a win scenario """
 def on_win():
-    """ Code handling a win scenario """
     # You code here
     basic.show_icon(IconNames.HAPPY)
+""" Code handling a loss scenario """
 def on_loss():
-    """ Code handling a loss scenario """
     # You code here
     basic.show_icon(IconNames.SAD)
-
-def on_forever():
-    """ Main code, reading/writing and logic """
-    # You code here
-forever(on_forever)
