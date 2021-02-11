c160c10e695f7644718452123b3e85e1ee0db98e
laugo-forritun <77778082+laugo-forritun@users.noreply.github.com>
2021-02-11 10:31:53 +0000

    server.py: Updated to serve one client at a time, preventing crash

--- server.py
+++ server.py
@@ -5,60 +5,15 @@ def send(ID: number, n: number):
     """ Send `n` to client `ID` """
     radio.send_value(str(ID), n)
 
-# Custom implementation of hashmap since python
-# hashmap is borked in MicroBit
-#
-# map_init(n) - Initialize map of capacity n
-# map_put(m, k, v) - In map m, put v at k
-# map_get(m,k) - In map m, get value at k
-#
-# NOTE:
-# The values returned on invalid keys is undefined
-def map_hash(k: number, size: number):
-    """
-    Return hash of `k`, for a `size`-big container
-    """
-    return (k*BIG_PRIME) % (3*size)
-def map_put(m: List[number], k: number, v: number):
-    """ Put `v` at `k` in map `m` """
-    hashed_key = map_hash(k,len(m))
-    m[hashed_key] = v
-def map_get(m: List[number], k: number):
-    """ Get value at `k` in `m` """
-    hashed_key = map_hash(k,len(m))
-    return m[hashed_key]
-def map_init(size: number):
-    """ Initialize a map of size `size` """
-    """
-    size += 1 # Off by 1 in size, otherwise
-    _chunks = [[0]]
-    while len(_chunks[len(_chunks)-1])*10 < size:
-        _x = [0]; _x.pop()
-        for _ in range(10):
-            _x = _x+_chunks[len(_chunks)-1]
-        _chunks.push(_x)
-    _chunks.reverse()
-    _a = [0]; _a.pop()
-    for chunk in _chunks:
-        while len(_a)+len(chunk) < size:
-            _a = _a + chunk
-    """
-    _a = [0]
-    for _ in range(size):
-        _a.push(0)
-    print("Initialized a map with size " + len(_a))
-    return _a
 
 
 
 def clear_contestant(contestant):
     """ Remove `contestant` as a client """
-    for i in range(len(bits)):
-        if bits[i] == contestant:
-            bits.remove_at(i)
-            break
-    map_put(challenge_id, contestant, 0)
-    map_put(progress, contestant, 0)
+    global challenge, bit, progress
+    challenge = -1
+    bit = -1
+    progress = -1
 def send_win(contestant):
     """ Notify `contestant` that they won """
     radio.send_string("" + contestant + " WIN")
@@ -79,40 +34,47 @@ def send_loss(contestant):
 
 def on_received_value(name, value):
     """ Message handling """
+    global progress, challenge, bit
     ID = int(name)
 
     # Register contestant in `bits`,
     # and send challenge
-    if ID not in bits:
-        bits.push(ID)
-        map_put(challenge_id, ID, value)
-        map_put(progress, ID, 0)
-        for x in challenges[value]:
+    if bit == -1:
+        led.toggle(1,0)
+        bit = ID
+    if ID != bit:
+        # Only one bit allowed
+        led.toggle(2,0)
+        return
+
+    # Select challenge
+    if challenge == -1:
+        led.toggle(3,0)
+        challenge = value
+        # Send all challenge numbers
+        for x in challenges[challenge]:
+            led.toggle(0,4)
             send(ID, x)
-            pause(100)
         return
 
     # Increment progress, or terminate in loss
-    challenge = map_get(challenge_id, ID)
-    prog = map_get(progress, ID)
-    answer = answers[challenge][prog]
+    answer = answers[challenge][progress]
     if abs((answer ^ SECRET_KEY) - value) < EPSILON:
         # Increment progress
-        map_put(progress, ID, prog+1)
-        led.plot(prog%5, prog/5)
+        led.plot_bar_graph(progress, len(challenges[challenge]))
+        progress += 1
     else:
         send_loss(ID)
         return
 
     # Check for completion
-    prog = map_get(progress, ID)
-    challenge = map_get(challenge_id, ID)
     answer_len = len(answers[challenge])
-    if prog >= answer_len:
+    if progress >= answer_len:
         send_win(ID)
 
     # Show packet transmission rate
     led.toggle(3,4)
+
 
 
 
@@ -124,11 +86,10 @@ radio.set_group(1)
 SECRET_KEY = 123456789
 BIG_PRIME = 946290599
 EPSILON = 0.01
-DEFAULT_MAP_SIZE = 100
 
-challenge_id = map_init(DEFAULT_MAP_SIZE)
-progress = map_init(DEFAULT_MAP_SIZE)
-bits: List[number] = []
+challenge = -1
+progress = -1
+bit = -1
 
 ###########################################
 ### EDIT THE CHALLENGE AND ANSWERS LIST ###
@@ -254,10 +215,12 @@ answers: List[List[number]] = [
 # Show the server is running
 def on_forever():
     """ Prove server is running """
-    led.toggle(4,4)
-    for i in range(len(bits)):
-        led.toggle(i%5,i/5)
-    basic.pause(250)
+    if bit != -1:
+        led.toggle(0,0)
+    for _ in range(4):
+        led.toggle(4,4)
+        pause(50)
+    basic.pause(400)
 
 forever(on_forever)
 radio.on_received_value(on_received_value)
